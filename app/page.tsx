56701e123eaae1f1cadd2e3b71b68f170a6a7fcf
Paolo Flores <maximo_150@hotmail.com>
2026-06-09 15:14:00 -0500

    Fetching Data

--- app/page.tsx
+++ app/page.tsx
@@ -17,17 +17,17 @@ export default function Page() {
           <p
             className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
+            <strong>Bienvenido a Acme.</strong> Este es el ejemplo del{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              curso de aprendizaje de Next.js
             </a>
-            , brought to you by Vercel.
+            , presentado por Vercel.
           </p>
           <Link
-            href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            href="/login"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Iniciar sesión</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,37 +1,37 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
+import { ArrowPathIcon } from "@heroicons/react/24/outline"
+import clsx from "clsx"
+import Image from "next/image"
+import { lusitana } from "@/app/ui/fonts"
+import { LatestInvoice } from "@/app/lib/definitions"
 export default async function LatestInvoices({
   latestInvoices,
 }: {
-  latestInvoices: LatestInvoice[];
+  latestInvoices: LatestInvoice[]
 }) {
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Latest Invoices
+        Últimas facturas
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         {/* NOTE: Uncomment this code in Chapter 7 */}
 
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
                 key={invoice.id}
                 className={clsx(
-                  'flex flex-row items-center justify-between py-4',
+                  "flex flex-row items-center justify-between py-4",
                   {
-                    'border-t': i !== 0,
+                    "border-t": i !== 0,
                   },
                 )}
               >
                 <div className="flex items-center">
                   <Image
                     src={invoice.image_url}
-                    alt={`${invoice.name}'s profile picture`}
+                    alt={`${invoice.name}"s profile picture`}
                     className="mr-4 rounded-full"
                     width={32}
                     height={32}
@@ -51,14 +51,14 @@ export default async function LatestInvoices({
                   {invoice.amount}
                 </p>
               </div>
-            );
+            )
           })}
-        </div> */}
+        </div>
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+          <h3 className="ml-2 text-sm text-gray-500 ">Actualizado hace un momento</h3>
         </div>
       </div>
     </div>
-  );
+  )
 }
