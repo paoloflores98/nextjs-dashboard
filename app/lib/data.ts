b54f03efff0f9e890cbde0323a0de106e6a67e22
Paolo Flores <maximo_150@hotmail.com>
2026-06-09 16:46:36 -0500

    Streaming

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,25 +11,17 @@ import { formatCurrency } from "./utils"
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" })
 
-// Obtener las últimas 5 facturas, ordenadas por fecha
-const data = await sql<LatestInvoiceRaw[]>`
-  SELECT invoices.amount, customers.name, customers.image_url, customers.email
-  FROM invoices
-  JOIN customers ON invoices.customer_id = customers.id
-  ORDER BY invoices.date DESC
-  LIMIT 5`
-
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don"t do this in production :)
 
-    // console.log("Fetching revenue data...")
-    // await new Promise((resolve) => setTimeout(resolve, 3000))
+    console.log("Fetching revenue data...")
+    await new Promise((resolve) => setTimeout(resolve, 3000))
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`
 
-    // console.log("Data fetch completed after 3 seconds.")
+    console.log("Data fetch completed after 3 seconds.")
 
     return data
   } catch (error) {
@@ -38,8 +30,11 @@ export async function fetchRevenue() {
   }
 }
 
+// Obtener las últimas 5 facturas, ordenadas por fecha
 export async function fetchLatestInvoices() {
   try {
+    await new Promise((resolve) => setTimeout(resolve, 3000))
+
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
